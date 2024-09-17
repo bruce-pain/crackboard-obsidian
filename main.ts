6bf6f7aabd635c538f449447524f8864e1f5efbb
Franklin Ikeh <ikehfranklin06@gmail.com>
2024-09-17 12:50:08 +0100

    refactor

--- main.ts
+++ main.ts
@@ -1,14 +1,8 @@
-import { MarkdownView, Plugin, App, PluginSettingTab, Setting } from 'obsidian';
+import { MarkdownView, Plugin, App, PluginSettingTab, Setting, requestUrl } from 'obsidian';
 
 // Constants
 const HEARTBEAT_INTERVAL = 2 * 60 * 1000; // 2 minutes in milliseconds
 const ENDPOINT = 'http://crackboard.dev/heartbeat';
-
-// Variables
-let lastHeartbeatTime: number | undefined;
-let typingTimer: number | undefined;
-let sessionKey: string | undefined;
-
 
 interface CrackboardPluginSettings {
 	sessionKey: string;
@@ -21,9 +15,14 @@ const DEFAULT_SETTINGS: CrackboardPluginSettings = {
 export default class CrackboardPlugin extends Plugin {
 	settings: CrackboardPluginSettings;
 
+	// Variables
+	private lastHeartbeatTime: number | undefined;
+	private typingTimer: number | undefined;
+
 	private async sendHeartbeat(language: string) {
 		try {
-			const response = await fetch(ENDPOINT, {
+			const response = requestUrl({
+				url: ENDPOINT,
 				method: 'POST',
 				headers: { 'Content-Type': 'application/json' },
 				body: JSON.stringify({
@@ -33,17 +32,15 @@ export default class CrackboardPlugin extends Plugin {
 				})
 			});
 
-			if (response.ok) {
-				lastHeartbeatTime = Date.now();
+			if ((await response).status == 200) {
+				this.lastHeartbeatTime = Date.now();
 				console.log('Heartbeat sent successfully.');
 			} else {
-				console.error('Failed to send heartbeat:', response.statusText);
+				console.error('Failed to send heartbeat:', (await response).status);
 			}
 		} catch (error) {
 			console.error('Failed to send heartbeat:', error);
 		}
-
-		// console.log(`Heartbeat sent successfully.\nSession key: ${this.settings.sessionKey}`);
 
 	};
 
@@ -54,16 +51,16 @@ export default class CrackboardPlugin extends Plugin {
 
 		this.registerEvent(
 			this.app.workspace.on("editor-change", () => {
-				if (typingTimer) {
-					window.clearTimeout(typingTimer)
+				if (this.typingTimer) {
+					window.clearTimeout(this.typingTimer)
 				}
 
-				typingTimer = window.setTimeout(() => {
+				this.typingTimer = window.setTimeout(() => {
 					const now = Date.now()
 					const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
 					if (activeView) {
 						const language = "markdown";
-						if (!lastHeartbeatTime || (now - lastHeartbeatTime) >= HEARTBEAT_INTERVAL) {
+						if (!this.lastHeartbeatTime || (now - this.lastHeartbeatTime) >= HEARTBEAT_INTERVAL) {
 							this.sendHeartbeat(language);
 						}
 					}
@@ -71,6 +68,12 @@ export default class CrackboardPlugin extends Plugin {
 			})
 		)
 
+	}
+
+	onunload(): void {
+		if (this.typingTimer) {
+			window.clearTimeout(this.typingTimer);
+		}
 	}
 
 	async loadSettings() {
